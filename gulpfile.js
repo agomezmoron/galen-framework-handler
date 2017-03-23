e26244fa582a3917e2aef75561f538e18e402cc8
Alejandro Gómez Morón <amoron@emergya.com>
2017-03-23 10:11:39 +0100

    Updating the version fixing multidevices

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
     concat = require('gulp-concat');
 
 gulp.task('release', function() {
-    return gulp.src(['./src/browsers.js','./src/commons.js', '../src/configuration.js', '../src/galen-overriding.js'])
+    return gulp.src(['./src/browsers.js','./src/commons.js', './src/configuration.js', './src/galen-overriding.js'])
         .pipe(concat('galen-framework-handler.js'))
         .pipe(gulp.dest('./dist/'));
 });
